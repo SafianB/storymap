e9b61a02191d4d97571f88d74eb86485d8ce073b
SafianB <safinkarunia93@gmail.com>
2025-05-04 14:47:29 +0700

    initial commit

--- leaflet.js
+++ leaflet.js
@@ -1,5 +1,5 @@
 // Initialize the map centered on your location
-const map = L.map('map').setView([-6.593077, 140.548782], 16); // Replace with your coordinates
+const map = L.map('map').setView([-6.593077, 140.548782], 10); // Replace with your coordinates
     
 // Add OpenStreetMap tiles with proper attribution
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -7,10 +7,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 20
 }).addTo(map);
 
-// Add a marker
 L.marker([-6.593077, 140.548782]) // Replace with your coordinates
   .addTo(map)
-  .bindPopup("<b>Camp 19,Butiptiri,</b><br>Boven Digoel,<br>Papua, Indonesia.");
+  .bindPopup("<b>Camp 19, Butiptiri,</b><br>Boven Digoel,<br>Papua, Indonesia.");
+  
+  L.marker([-6.618333, 140.278886]) // Replace with your coordinates
+  .addTo(map)
+  .bindPopup("<b>Camp 18, Gentiri,</b><br>Boven Digoel,<br>Papua, Indonesia.");
+  
+  L.marker([-6.418053, 140.390114]) // Replace with your coordinates
+  .addTo(map)
+  .bindPopup("<b>Camp 17, Miri,</b><br>Boven Digoel,<br>Papua, Indonesia.");
   
 
 // Optional: Add a circle for visual interest
